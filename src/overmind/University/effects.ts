98e38b6c48f3bd29743038b313397c1039a9baf6
asmaa3107 <asmaa3107@gmail.com>
2023-01-06 14:29:13 +0200

    remove comments

--- src/overmind/University/effects.ts
+++ src/overmind/University/effects.ts
@@ -3,8 +3,6 @@ import axios  from 'axios';
 import { university } from './states'
   export const httpApi = {
     getAll: async (): Promise<university[]> => {
-        // const data:university [] = await (await axios.get('http://universities.hipolabs.com/search?country=United+States')).data;
-        // console.log(data);
-    return (await axios.get('http://universities.hipolabs.com/search?country=United+States')).data;
+      return (await axios.get('http://universities.hipolabs.com/search?country=United+States')).data;
     }
 }
